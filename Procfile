eb111217497ca2da751042a8241f0f60eb011754
Enrico Hoffmann <enrico.hoffmann@movingimage24.com>
2015-02-13 00:35:52 +0100

    * add express * edit Procfile for new serve.js

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node web/js/app.js
+web: node serve.js
---
+++ serve.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var serve = express();
+serve.use(express.static(__dirname + '/web'));
+serve.set('port', (process.env.PORT || 3000));
+serve.listen(serve.get('port'), function() {
+  console.log("Node app is running at localhost:" + serve.get('port'));
+});
