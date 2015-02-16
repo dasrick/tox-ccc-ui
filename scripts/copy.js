8674d3e1aa819c46a4aa08f215c428210a59fc18
Enrico Hoffmann <enrico.hoffmann@movingimage24.com>
2015-02-16 19:09:55 +0100

    remove 'fs' and 'path' ... and add 'fs-extra' ... maybe it fix the heroku deployment issue

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,25 +1,30 @@
 var path = require('path');
-var fs = require('fs');
+var fs = require('fs-extra');
 var glob = require("glob");
 
 // options is optional
 glob(process.argv[2], {}, function (er, files) {
   files.forEach(function(file) {
     var destFile = path.normalize(rename(process.argv[3], file));
-    mkdirp(path.dirname(destFile));
-    fs.writeFileSync(destFile, fs.readFileSync(file));
+    //mkdirp(path.dirname(destFile));
+    //fs.writeFileSync(destFile, fs.readFileSync(file));
+    fs.copy(file, destFile, function(err) {
+      if (err) {
+        return console.error(err);
+      }
+    });
   });
 });
 
-function mkdirp(dir) {
-  var newDir = path.resolve(__dirname + '/../');
-  for (var i = 0; i < dir.split('/').length; i++) {
-    newDir = path.join(newDir, dir.split('/')[i]);
-    if (!fs.existsSync(newDir)) {
-      fs.mkdir(newDir)
-    }
-  }
-}
+//function mkdirp(dir) {
+//  var newDir = path.resolve(__dirname + '/../');
+//  for (var i = 0; i < dir.split('/').length; i++) {
+//    newDir = path.join(newDir, dir.split('/')[i]);
+//    if (!fs.existsSync(newDir)) {
+//      fs.mkdir(newDir)
+//    }
+//  }
+//}
 
 function rename(dest, src) {
   var srcPath = src.split('/').reverse();
