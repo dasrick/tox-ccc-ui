ec8c1eccb5d3af51fa97459d5c398d62981ffca5
Enrico Hoffmann <enrico.hoffmann@movingimage24.com>
2015-02-26 19:47:25 +0100

    cleanup copy script

--- scripts/copy.js
+++ scripts/copy.js
@@ -2,31 +2,21 @@ var path = require('path');
 var fs = require('fs-extra');
 var glob = require("glob");
 
+function rename(dest, src) {
+  var srcPath = src.split('/').reverse();
+  return dest + srcPath[2] + '/' + srcPath[0];
+}
+
 // options is optional
 glob(process.argv[2], {}, function (er, files) {
-  files.forEach(function(file) {
+  files.forEach(function (file) {
     var destFile = path.normalize(rename(process.argv[3], file));
     //mkdirp(path.dirname(destFile));
     //fs.writeFileSync(destFile, fs.readFileSync(file));
-    fs.copy(file, destFile, function(err) {
+    fs.copy(file, destFile, function (err) {
       if (err) {
         return console.error(err);
       }
     });
   });
 });
-
-//function mkdirp(dir) {
-//  var newDir = path.resolve(__dirname + '/../');
-//  for (var i = 0; i < dir.split('/').length; i++) {
-//    newDir = path.join(newDir, dir.split('/')[i]);
-//    if (!fs.existsSync(newDir)) {
-//      fs.mkdir(newDir)
-//    }
-//  }
-//}
-
-function rename(dest, src) {
-  var srcPath = src.split('/').reverse();
-  return dest + srcPath[2] + '/' + srcPath[0];
-}
