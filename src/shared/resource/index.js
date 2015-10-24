b5d3af572c198fc59ec040ff8e6e7a070751bfca
Enrico Hoffmann <dasrick@gmail.com>
2015-10-24 22:35:05 +0200

    playing with profile, but ... read TODO in resoucre config

--- src/shared/resource/index.js
+++ src/shared/resource/index.js
@@ -10,6 +10,7 @@ angular.extend(resource, require('./log'));
 angular.extend(resource, require('./plan'));
 angular.extend(resource, require('./player-skin'));
 angular.extend(resource, require('./product'));
+angular.extend(resource, require('./profile'));
 angular.extend(resource, require('./review'));
 angular.extend(resource, require('./transcoder-profile'));
 angular.extend(resource, require('./user'));
