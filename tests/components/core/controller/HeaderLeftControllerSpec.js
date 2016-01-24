0cd89a3225ef9970e23b0518b5bbca096dddfea2
Enrico Hoffmann <dasrick@gmail.com>
2016-01-24 09:19:48 +0100

    fix tmp. unused stuff in test of header (reported by codacy)

--- tests/components/core/controller/HeaderLeftControllerSpec.js
+++ tests/components/core/controller/HeaderLeftControllerSpec.js
@@ -4,16 +4,17 @@ var HeaderLeftController = require('../../../../src/components/core/controller/H
 
 describe('Components:Core:Controller:HeaderLeftController', function () {
 
-  var createController, $q, $rootScope, locals;
+  //var createController, $q, $rootScope, locals;
+  var createController;
 
   var CurrentUserService = jasmine.createSpyObj('CurrentUserService', ['getSelectedCustomer']);
 
   beforeEach(function () {
     angular.mock.inject(function ($injector) {
       var $controller = $injector.get('$controller');
-      $q = $injector.get('$q');
-      $rootScope = $injector.get('$rootScope');
-      locals = {
+      //var $q = $injector.get('$q');
+      //var $rootScope = $injector.get('$rootScope');
+      var locals = {
         CurrentUserService: CurrentUserService
       };
       createController = function () {
